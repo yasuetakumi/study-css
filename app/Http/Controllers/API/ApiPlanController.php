d16468caecca338db8c079af7df28fd299d42111
Komang <komang@gruneasia.info>
2022-01-28 13:30:37 +0800

    fix estimation index form

--- app/Http/Controllers/API/ApiPlanController.php
+++ app/Http/Controllers/API/ApiPlanController.php
@@ -12,4 +12,8 @@ class ApiPlanController extends Controller
         $plan = Plan::select('display_name','id', 'internal_id', 'area_group_id')->where('design_category_id',$category_id)->get();
         return response()->json($plan);
     }
+    public function getPlanByAreaGroup($category_id, $area_id){
+        $plan = Plan::select('display_name','id', 'area_group_id')->where('design_category_id',$category_id)->where('area_group_id', $area_id)->get();
+        return response()->json($plan);
+    }
 }
