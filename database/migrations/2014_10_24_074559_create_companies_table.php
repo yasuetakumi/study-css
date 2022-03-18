746d32b94681e65c52c84f89141aa8c2ec75dd12
mavindraadiyasa <mavindra@gruneasia.info>
2022-03-18 11:27:13 +0800

    [DB-10] Update Companies table with 6 additional fields - add new columns on Company Migration - update additional fields to fillable on Company Model - update seeder for company

--- database/migrations/2014_10_24_074559_create_companies_table.php
+++ database/migrations/2014_10_24_074559_create_companies_table.php
@@ -18,9 +18,15 @@ class CreateCompaniesTable extends Migration
             $table->unsignedBigInteger('company_admin_id')->nullable();
             $table->foreign('company_admin_id')->references('id')->on('admins')->onUpdate('cascade')->onDelete('set null');
             $table->string('company_name',50);
+            $table->string('company_name_kana')->nullable();
+            $table->string('agent_license_name')->nullable();
+            $table->string('agent_license_renewals')->nullable();
+            $table->string('agent_license_number')->nullable();
             $table->string('post_code',7);
             $table->string('address',100);
             $table->string('phone',20);
+            $table->string('fax')->nullable();
+            $table->string('url')->nullable();
             $table->string('status', 50);
             $table->timestamps();
             $table->softDeletes();
