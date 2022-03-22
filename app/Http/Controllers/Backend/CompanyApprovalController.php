4bc854d2356212b9b58611710b514c244f1277d8
Komang <komang@gruneasia.info>
2022-03-22 17:33:40 +0800

    - move function edit company to function show, for fix url A17 - adjust company form disabled input if route == company approval show

--- app/Http/Controllers/Backend/CompanyApprovalController.php
+++ app/Http/Controllers/Backend/CompanyApprovalController.php
@@ -22,10 +22,18 @@ class CompanyApprovalController extends Controller
     {
         if( $param == 'json' ){
             $model = Company::where('status', Company::PENDING);
-            return (new DatatablesHelper)->instance($model, true, false)->toJson();
+            return (new DatatablesHelper)->instance($model, false, false, true)->toJson();
 
         }
-        abort(404);
+        // abort(404);
+        $id = $param;
+        $data['item']           = Company::with('admin')->where('companies.id', $id)->first();
+
+        $data['page_title']     = __('label.edit') . __('label.company_approval_list');
+        $data['form_action']    = route('admin.approval.update', $id);
+        $data['page_type']      = 'edit';
+
+        return view('backend.company.form', $data);
     }
 
     /**
@@ -76,13 +84,7 @@ class CompanyApprovalController extends Controller
      */
     public function edit($id)
     {
-        $data['item']           = Company::with('admin')->where('companies.id', $id)->first();
 
-        $data['page_title']     = __('label.edit') . __('label.company_approval_list');
-        $data['form_action']    = route('admin.approval.update', $id);
-        $data['page_type']      = 'edit';
-
-        return view('backend.company.form', $data);
     }
 
     /**
