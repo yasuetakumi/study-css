0fedf38fef79048cb49f5c66e25b02943033affe
Ridwan-Grune <79817044+Ridwan-Grune@users.noreply.github.com>
2022-04-11 18:10:15 +0800

    [Command 2 + Email-5] Check remaining publishable days and bill the company if they are overdue - add new condition i. Reduce companies.remaining_points by  $cost_to_publish - ii. Add new property status period: proeprty_publication_status_period.remaining_days = 30, status = PUBLISHED, status_start_date= current date - iii. Disable old property status period: set status_end_date to current time. Set is_current_status to FALSE/0

--- app/Console/Commands/CheckRemainingPublishDayAndBillClient.php
+++ app/Console/Commands/CheckRemainingPublishDayAndBillClient.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Mail;
 use Carbon\Carbon;
 use App\Mail\WarningCompanyFailToPayToRenewProperty;
 // -----------------------------------------------------------------------------
+use App\Models\Company;
 use App\Models\Property;
 use App\Models\PropertyPublicationStatus;
 use App\Models\PropertyPublicationStatusPeriod;
@@ -72,11 +73,12 @@ class CheckRemainingPublishDayAndBillClient extends Command
                 //get date difference Calculate the remaining days for each property
                 $today = Carbon::now()->format('Y/m/d');
                 $startDate = Carbon::parse($property->status_start_date);
-                $remainingDay = $startDate->diffInDays($today, false) + 1;
+                $dateDiff = $startDate->diffInDays($today, false) + 1;
+                $remainingDay = ($property->remaining_publication_days) - ($dateDiff) + 1;
 
                 //logging----------------
-                print "processing calculate for properties id " . $property->id . ": (".$property->remaining_publication_days.") - (".$today." - ".$startDate.")+1 = ".$remainingDay."days \n";
-                \Log::info("processing calculate for properties id " . $property->id . ": (".$property->remaining_publication_days.") - (".$today." - ".$startDate.")+1 = ".$remainingDay."days");
+                print "processing calculate for properties id " . $property->id . ": (".$property->remaining_publication_days.") - (".$today." - ".$startDate.") ".$dateDiff." +1 = ".$remainingDay."days \n";
+                \Log::info("processing calculate for properties id " . $property->id . ": (".$property->remaining_publication_days.") - (".$today." - ".$startDate.") ".$dateDiff." +1 = ".$remainingDay."days");
 
                 //3. IF the remaining days is less than or equal to 0:
                 if($remainingDay <= 0){
@@ -86,13 +88,27 @@ class CheckRemainingPublishDayAndBillClient extends Command
 
                     //check company point first
                     $publishCost = ENV('PUBLISH_COST') ? ENV('PUBLISH_COST') : 150; //get publish cost from env, if there isn't any charge default by 150
-                    $companyPoint = $property->user && $property->user->company ? $property->user->company->remaining_points : 0;
+                    $findCompany = Company::where('id', $property->user->belong_company_id)->first();
+                    $companyPoint = $findCompany ? $findCompany->remaining_points : 0;
                     print "companies " . $property->user->belong_company_id . " points is : ".$companyPoint."\n";
                     \Log::info("companies " . $property->user->belong_company_id . " points is : ".$companyPoint);
 
                     //Condition A. If that company has points >= $cost_to_publish: 
                     if($companyPoint>=$publishCost){
                         //Spend those points and update the property status periods log
+                        //i. Reduce companies.remaining_points by  $cost_to_publish 
+                        $companyPointRemaining = $companyPoint - $publishCost;
+                        //update the company data
+                        $dataUpdate['remaining_points'] = $companyPointRemaining;
+                        $findCompany->update($dataUpdate);
+                        
+                        print "udpated companies point : " . $companyPointRemaining . " Remaining \n";
+                        \Log::info("udpated companies point : " . $companyPointRemaining . " Remaining");
+
+                        //ii. Add new property status period: proeprty_publication_status_period.remaining_days = 30, status = PUBLISHED, status_start_date= current date:
+                        //iii. Disable old property status period: set status_end_date to current time. Set is_current_status to FALSE/0 
+                        $this->updateCompanyPublish($property->id, 30, PropertyPublicationStatus::ID_PUBLISHED);
+
                     }
 
                     //Condition B. If that company has points < $cost_to_publish: try to automatically charge the company by credit card: use the details in the payment_details table to charge to customer 
@@ -139,7 +155,47 @@ class CheckRemainingPublishDayAndBillClient extends Command
         }
     }
 
-    public function ChargeCompany(){
-
+    public function updateCompanyPublish($propertyId, $remainingPublicationDay, $publicationStatusId){
+        try{
+            \DB::beginTransaction();
+            $property = Property::find($propertyId);
+    
+            $property->update([
+                'publication_status_id' => $publicationStatusId,
+            ]);
+            
+            $previous_period = PropertyPublicationStatusPeriod::where('property_id', $propertyId)->where('is_current_status', 1)->first();
+    
+            //ii. Add new property status period: proeprty_publication_status_period.remaining_days = 30, status = PUBLISHED, status_start_date= current date:
+            $property_period = new PropertyPublicationStatusPeriod();
+            $property_period->property_id = $propertyId;
+            $property_period->status_start_date = Carbon::now()->format("Y-m-d");
+            $property_period->status_end_date = null;
+            $property_period->is_current_status = true;
+            $property_period->remaining_publication_days = $remainingPublicationDay;
+            $property_period->publication_status_id = $publicationStatusId;
+            $property_period->save();
+    
+            if($previous_period){
+                //iii. Disable old property status period: set status_end_date to current time. Set is_current_status to FALSE/0 
+                if($property_period){
+                    $previous_period->update([
+                        'status_end_date' => Carbon::now()->format("Y-m-d"),
+                        'is_current_status' => false
+                    ]);
+                }
+            }
+            \DB::commit();
+            print "udpated companies publish remaining : 30 publication day and status become PUBLISHED, and disabled old status period \n";
+            \Log::info("udpated companies publish remaining  : 30 publication day and status become PUBLISHED, and disabled old status period");
+            
+            return true;
+        } catch (\Exception $e) {
+            \DB::rollback();
+            print "failed update companies:".$propertyId."\n";
+            \Log::info("failed update companies:".$propertyId);
+            return false;
+        }
+        
     }
 }
