30ef579fbee5d548528811935c54d41708db93f3
Komang <komang@gruneasia.info>
2022-04-19 14:53:57 +0800

    - modify walking distance filter - add const to walking distance from station option model

--- app/Models/WalkingDistanceFromStationOption.php
+++ app/Models/WalkingDistanceFromStationOption.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class WalkingDistanceFromStationOption extends Model
 {
+    const ID_1MINUTE = 1;
+    const ID_3MINUTE = 2;
+    const ID_5MINUTE = 3;
+    const ID_10MINUTE = 4;
+    const ID_15MINUTE = 5;
+    const ID_16MINUTEORMORE = 6;
+
     protected $fillable = [
         'value',
         'label_en',
