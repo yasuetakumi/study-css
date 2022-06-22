667e9930aea936f2a004133b4b93b2c0155ef322
AdityaShinduSun <aditya.shindu@sun-asterisk.com>
2022-06-22 18:38:01 +0900

    [FB-C2-5] [C-4] Replace the Property History List Item with common list item+ other changes - Use C-Common-3 component for "最新の閲覧履歴" - Added favorite button on the items in the "最新の閲覧履歴" - Change label "最新の閲覧履歴" -> "最近の閲覧履歴".

--- resources/views/frontend/property/component/visited-property.blade.php
+++ resources/views/frontend/property/component/visited-property.blade.php
@@ -1,10 +1,12 @@
 <div class="px-1"  v-if="historyProperty" >
 
     <h5 class="mb-3">
-        最新の閲覧履歴
+        最近の閲覧履歴
     </h5>
     <div v-for="pd in historyProperty" :key="pd.id">
-        <property-related-list :property="pd"></property-related-list>
+        <property-list :property="pd">
+            <button-favorite :likes="items.like_property" :idproperty="pd.id" @click="setLikeProperty(pd.id)"></button-favorite>
+        </property-list>
     </div>
 
     <a href="{{route('property.history', ['favorite' => true ])}}" class="btn btn-primary px-4 py-2 d-block w-100 mt-3">
