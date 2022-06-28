065ba3bd2ac5ff30f8e9b009f1bfb256e71c0335
AdityaShinduSun <aditya.shindu@sun-asterisk.com>
2022-06-28 16:22:02 +0900

    [FB-Label-3] - Fix label "気に入り物件から削除しました" -> "お気に入り物件から削除しました".

--- resources/views/frontend/property/history.blade.php
+++ resources/views/frontend/property/history.blade.php
@@ -289,7 +289,7 @@
                             console.log("index", index);
                             properties_like.splice(index, 1);
                             localStorage.setItem('favoritePropertyId', JSON.stringify(properties_like));
-                            let msg = '気に入り物件から削除しました。'; //remove like
+                            let msg = 'お気に入り物件から削除しました'; //remove like
                             this.$toasted.show( msg, {
                                 type: 'success'
                             });
@@ -322,7 +322,7 @@
                             console.log("index", index);
                             properties_like.splice(index, 1);
                             localStorage.setItem('favoritePropertyId', JSON.stringify(properties_like));
-                            let msg = '気に入り物件から削除しました。'; //remove like
+                            let msg = 'お気に入り物件から削除しました'; //remove like
                             this.$toasted.show( msg, {
                                 type: 'success'
                             });
