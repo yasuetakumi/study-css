d22b12a9a70acdde6ae6bd9b2a069301b86f19bc
komang <komang@gruneasia.info>
2022-09-16 16:49:07 +0800

    upgrade php 8

--- resources/views/frontend/_components/search_condition_list.blade.php
+++ resources/views/frontend/_components/search_condition_list.blade.php
@@ -307,7 +307,8 @@
                 }
             },
             convertToDateOnly: function(date){
-                return moment(date).format('YYYY-MM-DD');
+                let newDate = new Date(date);
+                return newDate.toISOString().split('T')[0]
             },
 
         }
