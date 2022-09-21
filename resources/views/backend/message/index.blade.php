8d9ffd7f92a7821ce812d56de09cc4448d56dc92
Komang <komang@gruneasia.info>
2022-09-21 18:00:12 +0800

    update webhook

--- resources/views/backend/message/index.blade.php
+++ resources/views/backend/message/index.blade.php
@@ -22,31 +22,14 @@
 
             @endcomponent
         </div>
-
         <div class="col-md-6">
-            <h3 class="text-center">Send Message To User</h3>
-            @component('backend._components.form_container', ["action" => $form_action2, 'id' => 'chat-form',  "page_type" => $page_type, "files" => false])
-                @component('backend._components.input_text', ['name' => 'message', 'label' => 'Message', 'required' => 1, 'value' => '']) @endcomponent
-                {{-- @component('backend._components.input_text', ['name' => 'title', 'label' => __('label.title'), 'required' => 1, 'value' => '']) @endcomponent --}}
-                <div class="card-footer text-center">
-                    <button type="submit" class="btn btn-secondary" id="input-submit">
-                        <i class="fas fa-save"></i> Broadcast Message
-                    </button>
-                </div>
-
-            @endcomponent
-        </div>
-
-
-    </div>
-    <div class="row mt-5">
-        <div class="col-md-4">
             <button @click="getBotInformation" class="btn btn-secondary" id="input-submit">
                 <i class="fas fa-save"></i> Get Bot Info
             </button>
-            <p>Bot Info @{{items.botInformation}}</p>
-            <ul v-if="items.botInformation">
-                <li v-for="(value, name, index) in items.botInformation">@{{name}} : @{{value}}</li>
+            <p>Bot Info </p>
+            <ul >
+                <li>ID : @{{basicId}}</li>
+                <li>Name : @{{displayName}}</li>
             </ul>
         </div>
     </div>
@@ -111,12 +94,8 @@
                 // ----------------------------------------------------------
                 // Form result set here
                 // ----------------------------------------------------------
-                items: {
-                    message_broadcast: '',
-                    message: '',
-                    user_id: '',
-                    botInformation: null,
-                },
+                basicId: '',
+                displayName: '',
                 // ----------------------------------------------------------
             };
             // --------------------------------------------------------------
@@ -174,10 +153,12 @@
 
             getBotInformation: function()
             {
+                var self = this;
                 axios.get(this.$store.state.preset.api.botInfo)
                 .then(function (response) {
-                    this.items.botInformation = response.data;
-                    console.log(response.data);
+                    self.basicId = response.data.basicId;
+                    self.displayName = response.data.displayName;
+                    console.log(response.data.basicId);
                 })
                 .catch(function (error) {
                     console.log(error);
