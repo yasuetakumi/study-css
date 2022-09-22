5382613560591f6c19d7b3ba1b326eb3804c6eb5
komang <komang@gruneasia.info>
2022-09-22 17:39:23 +0800

    - remove name furigana

--- resources/views/backend/member/form.blade.php
+++ resources/views/backend/member/form.blade.php
@@ -10,7 +10,6 @@
     @component('backend._components.form_container', ["action" => $form_action, "page_type" => $page_type, "files" => false])
         @component('backend._components.input_text', ['name' => 'company_name', 'label' => __('label.company_name'), 'required' => 0, 'value' => $item->company_name ?? '']) @endcomponent
         @component('backend._components.input_text', ['name' => 'name', 'label' => __('label.name'), 'required' => 1, 'value' => $item->name ?? '']) @endcomponent
-        @component('backend._components.input_text', ['name' => 'name_furigana', 'label' => __('label.name_furigana'), 'required' => 0, 'value' => $item->name_furigana ?? '', 'onlyjapanese' => true]) @endcomponent
         @component('backend._components.input_text', ['name' => 'name_kana', 'label' => __('label.name_kana'), 'required' => 0, 'value' => $item->name_kana ?? '', 'onlykatana' => true]) @endcomponent
         @component('backend._components.input_phone_number', ['name' => 'phone_number', 'label' => __('label.phone'), 'required' => 0, 'value' => $item->phone_number ?? '']) @endcomponent
         @component('backend._components.input_email', ['name' => 'email', 'label' => __('label.email'), 'required' => 1, 'value' => $item->email ?? '']) @endcomponent
