a06216a90234b0a6089fd541ff3cc2acfd2e08f3
komang <komang@gruneasia.info>
2022-09-23 15:21:25 +0800

    install provider line social lite

--- config/services.php
+++ config/services.php
@@ -45,9 +45,9 @@ return [
     ],
 
     'line' => [
-        'client_id'     => env('LINE_AUTH_CLIENT_ID'),
-        'client_secret' => env('LINE_AUTH_CLIENT_SECRET'),
-        'redirect'      => env('LINE_AUTH_REDIRECT'),
+        'client_id'     => env('LINE_CLIENT_ID'),
+        'client_secret' => env('LINE_CLIENT_SECRET'),
+        'redirect'      => env('LINE_REDIRECT_URI'),
     ],
 
 ];
